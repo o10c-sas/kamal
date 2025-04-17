9b29ac82c1f8f05bbe693a66587d23415082ad4e
David Besserman <dbesserman@o10c.local>
2025-04-17 09:58:10 +0200

    put docker login server url after options

--- lib/kamal/commands/registry.rb
+++ lib/kamal/commands/registry.rb
@@ -3,9 +3,9 @@ class Kamal::Commands::Registry < Kamal::Commands::Base
     registry_config ||= config.registry
 
     docker :login,
-      registry_config.server,
       "-u", sensitive(Kamal::Utils.escape_shell_value(registry_config.username)),
-      "-p", sensitive(Kamal::Utils.escape_shell_value(registry_config.password))
+      "-p", sensitive(Kamal::Utils.escape_shell_value(registry_config.password)),
+      registry_config.server
   end
 
   def logout(registry_config: nil)
